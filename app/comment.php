7714488c0be59c0c980f7292364cad6de86fdd75
idmc <id.morales10n@gmail.com>
2018-06-30 21:09:10 -0500

    generando login y register

--- app/comment.php
+++ app/comment.php
@@ -7,4 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 class comment extends Model
 {
     //
+    protected $table='comments';
+   //relacion a de Muchos a uno
+   public function user(){
+    return $this->belongsTo('App\User', 'user_id');
+ }
+
 }
--- app/video.php
+++ app/video.php
@@ -6,5 +6,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class video extends Model
 {
-    //
+    //enlasando a la tabla videos
+    protected $table='videos';
+    
+    //relacion one to many
+    public function comments(){
+        return $this->hasMany('App\Comment');
+    }
+   
+    //relacion a de Muchos a uno
+     public function user(){
+        return $this->belongsTo('App\User', 'user_id');
+     }
+
+
+
+
+
 }
