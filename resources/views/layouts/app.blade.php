6393902eb6f81080317a422032f7aba8c0707e25
idmc <id.morales10n@gmail.com>
2018-09-23 21:19:13 -0500

    modificando vista layout app

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -34,9 +34,10 @@
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
-
+                  
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
+                    <br>
                     <ul class="navbar-nav mr-auto">
                         <form action="{{url('/videoSearch')}}" class="form-inline" role="search">
                             <div class="form-group">
